9ad11743dcb9d7390de68491335ad8b25497d221
mojtaba <zarei.bs@gmail.com>
2017-01-27 14:24:29 +0330

    add esdoc

--- src/appComponent/threeDimensionControllers/dimensionsController/sinusController/sinusController.js
+++ src/appComponent/threeDimensionControllers/dimensionsController/sinusController/sinusController.js
@@ -4,17 +4,14 @@ import { connect } from 'preact-redux';
 import SinusControllerElement from './sinusControllerElement.js';
 
 /**
- * present list of products
  * @param {object} state global state which contains all state we created
  * @param {object} ownProps associated property in parent component
- * e.g:ownProps could be used for edit
  */
 const mapStateToProps = (state, ownProps) => {
 	return {...state.dimension};
 };
 
 /**
- * present list of products
  * @param {object} dispatch to run a reducer
  * @param {object} ownProps associated property in parent component
  */
