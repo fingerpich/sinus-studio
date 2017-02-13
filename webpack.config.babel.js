3b794e602b47630e31d880e6de67fafad07f5903
mojtaba <zarei.bs@gmail.com>
2017-02-13 18:13:55 +0330

    set title

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -61,7 +61,7 @@ module.exports = {
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify(ENV)
 		}),
-		new HtmlWebpackPlugin()
+		new HtmlWebpackPlugin({title:'Indulge Your Child'})
 	]).concat(ENV === 'production' ? [
 		new webpack.optimize.DedupePlugin(),
 		new webpack.optimize.OccurenceOrderPlugin()
