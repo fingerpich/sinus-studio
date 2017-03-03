9515672e0b2c23c8d1e7ef7b583c757c8cbd310f
mojtaba <zarei.bs@gmail.com>
2017-03-04 02:50:27 +0330

    public path fixed

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 set -o errexit # Exit on error
-DEST="./fingerpich.github.io/spiro3d/"
+DEST="./fingerpich.github.io/spiro3d"
+SRC="./build"
+
 npm run build
 git clone https://github.com/fingerpich/fingerpich.github.io.git
-cp ./build/index.html $DEST
-cp ./build/style.css $DEST
-cp ./build/bundle.js $DEST
-cp ./build/spiro3D_SW.js $DEST
-cp -R "./build/icons" $DEST
-cp ./src/manifest.webmanifest $DEST
+
+cp ./src/manifest.webmanifest $SRC
+rm $SRC/*.map
+cp -R $SRC/* $DEST
+
 (cd $DEST && git add . && git commit -a -m "deploy Spiro3D on io" && git push origin master)
 rm -rf ./fingerpich.github.io
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -10,7 +10,7 @@ module.exports = {
 
 	output: {
 		path: './build',
-		publicPath: '/',
+		publicPath: '',
 		filename: 'bundle.js'
 	},
 
