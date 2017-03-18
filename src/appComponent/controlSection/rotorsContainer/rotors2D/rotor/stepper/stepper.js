0a24be5bb3678223b5162207a8d05ac1b5526e0e
mojtaba <zarei.bs@gmail.com>
2017-03-18 14:04:44 +0330

    optimize stepper

--- src/appComponent/controlSection/rotorsContainer/rotors2D/rotor/stepper/stepper.js
+++ src/appComponent/controlSection/rotorsContainer/rotors2D/rotor/stepper/stepper.js
@@ -8,36 +8,43 @@ import "./stepper.less";
 class Stepper extends Component {
 	constructor() {
 		super();
-		this.state.count=0;
 	}
-
-	componentWillReceiveProps() {
-		this.state.count = this.props.value||0;
+	shouldComponentUpdate(data){
+		if(data.value!=this.value){
+			this.value=data.value;
+			this.textInput.value=this.value;
+		}
+		return false;
 	}
+	componentWillMount(){
+		this.value=this.props.value||0;
+		this.textInput.value=this.value;
+	}
+	value=0;
 	increase() {
-		this.setState({count:this.state.count+1});
+		this.value++;
 		this.onChange();
 	}
 	decrease() {
-		this.setState({count:this.state.count-1});
+		this.value--;
 		this.onChange();
 	}
 	onInputChange(e){
-		this.setState({count:e.target.value});
+		this.value=this.textInput.value;
 		this.onChange();
 	}
 	onChange(){
+		this.textInput.value=this.value;
 		if(this.props.onChange) {
-			this.props.onChange(this.props.name,this.state.count);
+			this.props.onChange(this.props.name,this.value);
 		}
 	}
 
 	render(props, state) {
 		return <div class="stepper">
-			<input class="number" type="number" value={props.value||this.state.count} onInput={this.onInputChange.bind(this)}/>
+			<input class="number" type="number" ref={(input) => { this.textInput = input; }} onInput={this.onInputChange.bind(this)}/>
 			<button class="minus" onClick={this.decrease.bind(this)}>-</button>
 			<button class="plus" onClick={this.increase.bind(this)}>+</button>
-
 		</div>;
 	}
 }
