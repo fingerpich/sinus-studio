479dcf9698551b39cbd821c2fa1f00ad7306d7ea
mojtaba <zarei.bs@gmail.com>
2017-03-19 01:59:54 +0330

    add help and fix problem

--- src/appComponent/controlSection/rotorsContainer/rotors2D/rotor/stepper/stepper.js
+++ src/appComponent/controlSection/rotorsContainer/rotors2D/rotor/stepper/stepper.js
@@ -1,25 +1,27 @@
 import {h, render, Component} from 'preact';
 import "./stepper.less";
 /**
- * Stepper Element
- * @return {object} presentation element
+ * Stepper Element used for change numbers in all device easily
+ * @example
+ * <Stepper/>
  */
-
 class Stepper extends Component {
-	constructor() {
-		super();
-	}
-
-	shouldComponentUpdate(data) {
-		if (data.value != this.value) {
-			this.value = data.value;
-			if (this.textInput) {
-				this.textInput.value = this.value;
-			}
+	/**
+	 * we don't need to rerender all over stepper when its property changes
+	 * @param {object} props
+	 */
+	shouldComponentUpdate(props) {
+		if (!this.textInput) return true;//it needs to render the element again
+		if(props.value != this.value){
+			this.value = props.value;
+			this.textInput.value = this.value;
 		}
 		return false;
 	}
 
+	/**
+	 * set value on and text value from props
+	 */
 	componentWillMount() {
 		this.value = this.props.value || 0;
 		this.textInput.value = this.value;
@@ -27,21 +29,35 @@ class Stepper extends Component {
 
 	value = 0;
 
+	/**
+	 * increase the value of stepper
+	 */
 	increase() {
 		this.value++;
 		this.onChange();
 	}
 
+	/**
+	 * decrease the value of stepper
+	 */
 	decrease() {
 		this.value--;
 		this.onChange();
 	}
 
+
+	/**
+	 * change value on text changed
+	 */
 	onInputChange(e) {
 		this.value = this.textInput.value;
 		this.onChange();
 	}
 
+
+	/**
+	 * call event for parents
+	 */
 	onChange() {
 		if (this.textInput) {
 			this.textInput.value = this.value;
@@ -51,6 +67,12 @@ class Stepper extends Component {
 		}
 	}
 
+
+	/**
+	 * render function
+	 * @param {object} props
+	 * @param {object} state
+	 */
 	render(props, state) {
 		return <div class="stepper">
 			<input class="number" type="number" ref={(input) => {
